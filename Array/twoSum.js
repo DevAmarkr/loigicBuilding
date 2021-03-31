c3293ae97564dd4aef7e5399c1fb0bad99d2ae0b
= <=>
2021-03-31 09:29:48 +0530

    two sum problem solved with hashmap

--- Array/twoSum.js
+++ Array/twoSum.js
@@ -23,5 +23,67 @@ When the map contain target - curNum, we know the search is complete
 The time complexity is O(n)
 The space complexity is O(n)
 
-
 */
+
+//1: O(n^2) Brute Force(worst)
+
+function  twoSum(arr,target){
+    for(let x = 0; x < arr.length; ++x){
+        for(let y = x+1; y < arr.length; ++y){
+            if(arr[x] + arr[y] === target){
+                  return [x,y] 
+            }
+        }
+    }
+    return 'Not found'
+}
+
+//2: O(n) time comlexity(Good)
+function twoSum(nums,target){
+    let map = new Map();
+    for (let i = 0; i < nums.length; i ++) {
+        if (map[nums[i]] >= 0) {
+            return [map[nums[i]], i]
+        }
+        map[target-nums[i]] = i
+        console.log(map)
+    }
+    return [-1, -1];
+}
+
+
+//3: O(n) time comlexity , expanded version of solution 2(more explanatory)
+const twoSum_On_Better = (arr, target) => {
+    if(!Array.isArray(arr) || typeof target !== 'number'){
+        return 'Invalid type'
+    }
+  
+    if(arr.length < 2){
+        return []
+    }
+
+    let hashMapTable = new Map()
+    //Loop for storing all elements of an array in hashmapTable as key and index of that element as value
+    for( let x = 0; x < arr.length; ++x){
+        hashMapTable[arr[x]] = x
+    }
+    //Loop for main logic to check if target- current value is presnt inside hashmap table or not
+    for(let y = 0; y < arr.length; ++y){
+        let needValue =  target - arr[y]
+        if(needValue in hashMapTable && hashMapTable[needValue] !== y){
+          return [y, hashMapTable[needValue]]
+        }
+    }
+    return [-1,-1]
+
+}
+
+
+
+
+
+
+
+
+
+
